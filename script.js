bff0522ea47831ae56bfaeecc716847f7bfa33ba
JeanPierre <jp1966.adoux@gmail.com>
2021-11-18 16:56:06 +0100

    Exercice Json le 18/11/2021 suite

--- script.js
+++ script.js
@@ -1,48 +1,40 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
-let requestURL = "./assets/json/movies.json";
-var request = new XMLHttpRequest();
+let requestURL = './assets/json/movies.json';
 
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function () {
-    var myFilm = request.response;
-    populateHeader(myFilm);
-    showFilm(myFilm);
-};
+fetch(requestURL)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.results[0].original_title);
+       data.results.forEach(element => {
+            let myOriginal_title = (element.original_title);
+            let myId = (element.id);
+            let myPoster_Path = (element.myPoster_Path);
+            let myVote_average = (element.vote_average);
+        });
+        console.log(myOriginal_title);
+        console.log(myId);
+        console.log(myPoster_Path);
+        console.log(myVote_average);
 
-function showFilm(jsonObj) {
-    var jsonFilm = jsonObj['members'];
+    });
 
-    for (var i = 0; i < jsonFilm.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myStars = document.createElement('p');
-        var myImage = document.createElement('p');
 
-        myH2.textContent = jsonFilm[i].original_title;
-        myPara1.textContent = jsonFilm[i].overview;
-        myStars.textContent = jsonFilm[i].vote_average;
-        myImage.textContent = jsonFilm[i].poster_path;
 
-        for (var j = 0; j < jsonFilm.length; j++) {
-            myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
-            myArticle.appendChild(myStars);
-            myArticle.appendChild(myImage);
-            section.appendChild(myArticle);
-        }
-    }
-};
-
-function populateHeader(jsonObj) {
-    var myH1 = document.createElement('h1');
-    myH1.textContent = jsonObj['original_title'];
-    header.appendChild(myH1);
-
-    var myPara = document.createElement('p');
-    myPara.textContent = 'overview : ' + jsonObj['overview'];
-    header.appendChild(myPara);
-}
+// function myJSON() {
+//  let jsonFilm = jsonObj['results'];
+//  for (let i = 0; i < jsonFilm.length; i++) {
+//   let myArticle = document.createElement('article');
+//   let myH2 = document.createElement('h2');
+//   let myPara1 = document.createElement('p');
+//   let myStars = document.createElement('p');
+//   let myImage = document.createElement('p');
+//   myH2.textContent = jsonFilm[i].original_title;
+//   myPara1.textContent = jsonFilm[i].overview;
+//   myStars.textContent = jsonFilm[i].vote_average;
+//   myImage.textContent = jsonFilm[i].poster_path;
+//   myArticle.appendChild(myH2);
+//   myArticle.appendChild(myPara1);
+//   myArticle.appendChild(myStars);
+//   section.appendChild(myArticle);
+//     }
